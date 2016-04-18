53aec2b1bc8492473ba9414936c25f51f3624fec
chadjefferies <chad.jefferies@gmail.com>
2016-04-18 01:52:05 -0400

    tried a max difference approach that failed

--- src/go/game-of-rotation/main.go
+++ src/go/game-of-rotation/main.go
@@ -5,7 +5,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"sort"
 	"strconv"
 )
 
@@ -16,6 +15,7 @@ func main() {
 		scanner := bufio.NewScanner(os.Stdin)
 		scanner.Split(bufio.ScanWords)
 		inputs := make([]int, n)
+
 		for j := 0; j < n; j++ {
 			if scanner.Scan() {
 				b, _ := strconv.Atoi(scanner.Text())
@@ -23,32 +23,92 @@ func main() {
 			}
 		}
 
-		sort.Ints(inputs)
-		fmt.Println(inputs)
 		pmean := 0
-		for i, v := range inputs {
-			if v < 0 {
+		for k := 0; k < n; k++ {
+			p := 0
+			for i, v := range inputs {
+				fmt.Println(p)
+				p = p + ((i + 1) * v)
+			}
+			if p > pmean {
+				pmean = p
+				//fmt.Println(pmean)
+				inputs = append(inputs, inputs[0])
+				inputs = append(inputs[:0], inputs[1:]...)
+				fmt.Println(inputs)
 				continue
 			}
-			pmean = pmean + ((i + 1) * v)
+
+			inputs = append(inputs, inputs[0])
+			inputs = append(inputs[:0], inputs[1:]...)
 		}
 
-		//fmt.Println(maxAt)
-
-		/*
-			for k := 0; k < n; k++ {
-				//inputs = append(inputs, inputs[0])
-				//inputs = append(inputs[:0], inputs[1:]...)
-				p := 0
-				for i, v := range inputs {
-					p = p + ((i + 1) * v)
-				}
-				if p > pmean {
-					pmean = p
-					break
-				}
-			}
-		*/
 		fmt.Println(pmean)
 	}
 }
+
+/*
+// https://www.hackerrank.com/challenges/game-of-rotation
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	var n int
+	_, err := fmt.Scan(&n)
+	if err == nil {
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Split(bufio.ScanWords)
+		inputs := make([]int64, n)
+
+		for j := 0; j < n; j++ {
+			if scanner.Scan() {
+				b, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+				inputs[j] = b
+			}
+		}
+
+		//		maxIdx := n - 1
+		startingIndex := 1
+		//		var maxDiff int64 = 0
+		//		for k := 0; k < n; k++ {
+		//			d := (inputs[maxIdx]) - (inputs[k])
+		//			if d > maxDiff {
+		//				maxDiff = d
+		//				startingIndex = k
+		//			}
+		//			if maxIdx == (n - 1) {
+		//				maxIdx = 0
+		//			} else {
+		//				maxIdx++
+		//			}
+		//		}
+
+		relativeIdx := startingIndex
+		var pmean int64 = 0
+		for i := 0; i < n; i++ {
+			v := inputs[relativeIdx]
+			fmt.Printf("%d ", pmean)
+			pmean = pmean + (int64(i+1) * v)
+			fmt.Printf("%d ", v)
+
+			if relativeIdx == (n - 1) {
+				relativeIdx = 0
+			} else {
+				relativeIdx++
+			}
+		}
+
+		fmt.Println()
+		fmt.Println(pmean)
+
+		//		inputs = append(inputs, inputs[0])
+		//		inputs = append(inputs[:0], inputs[1:]...)
+	}
+}
+*/
