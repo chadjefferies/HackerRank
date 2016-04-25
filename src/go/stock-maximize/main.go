82c504df82eb6f4fba6e443153f244d76b5169a6
chadjefferies <chad.jefferies@gmail.com>
2016-04-25 00:13:17 -0400

    used recursion and finally found a solution
    less than elegant

--- src/go/stock-maximize/main.go
+++ src/go/stock-maximize/main.go
@@ -15,66 +15,73 @@ func main() {
 		scanner := bufio.NewScanner(os.Stdin)
 		scanner.Split(bufio.ScanWords)
 		testCases := make([][]int, t)
+		testCaseMax := make([]int, t)
 		for i := 0; i < t; i++ {
 			if scanner.Scan() {
 				n, _ := strconv.Atoi(scanner.Text())
 				testCase := make([]int, n)
+				m, mi := 0, 0
 				for j := 0; j < n; j++ {
 					if scanner.Scan() {
 						c, _ := strconv.Atoi(scanner.Text())
 						testCase[j] = c
+						if c > m {
+							m = c
+							mi = j
+						}
 					}
 				}
+				testCaseMax[i] = mi
 				testCases[i] = testCase
 			}
 		}
 
-		for _, testCase := range testCases {
-			profit := 0
-			sharesBought := 0
-			for k := 0; k < len(testCase); k++ {
-				price := testCase[k]
-				sellPrice := price * sharesBought
-				if sellPrice == 0 {
-					sellPrice = price
+		for j, testCase := range testCases {
+			var profit int64
+			l := len(testCase) - 1
+
+			profit = getProfit(0, testCaseMax[j], testCase)
+			s := testCaseMax[j] + 1
+			e := l
+			for {
+				if s > e {
+					break
 				}
-				fmt.Printf("current: %d profit: %d sellPrice: %d\n", price, profit, sellPrice)
-
-				if k < len(testCase)-1 {
-					// look ahead to next item
-					nextPrice := testCase[k+1]
-
-					// what we could sell the next one for if we buy the current
-					nextSellPrice := nextPrice * (sharesBought + 2)
-
-					// buy current, sell next
-					profitSellNext := (profit - price) + nextSellPrice
-
-					// sell current
-					profitSellCurrent := profit + sellPrice
-
-					fmt.Printf("next price: %d next sell price: %d profit from selling next: %d profit from selling current: %d\n", nextPrice, nextSellPrice, profitSellNext, profitSellCurrent)
-
-					if profitSellCurrent > profitSellNext {
-						profit = profitSellCurrent
-						sharesBought++
-					}
-				} else {
-					// we are at the end of the list
-
-					// sell current
-					profit = profit + sellPrice
+				maxIdx := getMax(s, e, testCase)
+				profit = profit + getProfit(s, maxIdx, testCase)
+				if maxIdx == l {
+					break
 				}
+				s = maxIdx + 1
+			}
+			if profit < 0 {
+				profit = 0
 			}
 			fmt.Println(profit)
 		}
 	}
 }
 
-func buyOne(profit int, price int) int {
-	return profit - price
+func getProfit(startingIdx int, endingIdx int, testCase []int) int64 {
+	sharePrice := testCase[endingIdx]
+	cost := 0
+	sharesBought := 0
+	for k := startingIdx; k < endingIdx; k++ {
+		cost = cost + testCase[k]
+		sharesBought++
+	}
+	return int64(((sharePrice * sharesBought) - cost))
 }
 
-func sellShares(profit int, numShares int, price int) int {
-	return profit + (price * numShares)
+func getMax(startingIdx int, endingIdx int, testCase []int) int {
+	m := 0
+	mi := 0
+	for k := startingIdx; k <= endingIdx; k++ {
+		v := testCase[k]
+		if v > m {
+			m = v
+			mi = k
+		}
+	}
+	return mi
 }
