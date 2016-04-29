f61393ebffbf6b8c74cc9c05ff09245524c8732e
Chad Jefferies <cjefferies@listrak.com>
2016-04-29 14:05:40 -0400

    made the solution more functional
    replaced while loop with recursion

--- src/scala/super-digit/src/Solution.scala
+++ src/scala/super-digit/src/Solution.scala
@@ -1,22 +1,17 @@
-object Solution {
+ object Solution {
 
     def main(args: Array[String]) {
-      val input = scala.io.StdIn.readLine().split(" ")
+      val input = readLine().split(" ")
       val n = input(0)
       val k = input(1).toLong
 
-      var sd = n.map(c => c.asDigit*k)
-      var res = sd.foldLeft(0L)(_+_).toString
-      while (res.length > 1){
-        res = superDigit(res).toString()
-      }
-      println(res)
+      var first = n.map(c => c.asDigit*k)
+      var superDigit = getSuperDigit(first.sum.toString)
+      println(superDigit)
     }
     
-    def superDigit(a:String) : Long = {
-      val res = a.map(_.asDigit).toList
-      var sum: Long  = 0L
-      res.foreach(sum+=_)
-      return sum
+    def getSuperDigit(s:String) : String ={
+      if(s.length == 1) return s
+      return getSuperDigit(s.map(_.asDigit).foldLeft(0L)(_+_).toString)
     }
 }
