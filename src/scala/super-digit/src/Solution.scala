0276762de93cc02f33cfc5786c4b9f818e84078b
Chad Jefferies <cjefferies@listrak.com>
2016-05-06 15:50:26 -0400

    changed sum to foldleft

--- src/scala/super-digit/src/Solution.scala
+++ src/scala/super-digit/src/Solution.scala
@@ -5,7 +5,7 @@
       val n = input(0)
       val k = input(1).toLong
 
-      val superDigit = getSuperDigit(n.map(c => c.asDigit*k).sum.toString)
+      val superDigit = getSuperDigit(n.map(_.asDigit*k).foldLeft(0L)(_+_).toString)
       println(superDigit)
     }
     
